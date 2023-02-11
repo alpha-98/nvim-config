ecdfa758c33fff07a72d77f6bc8540d225ed95a2
Gambhir <grathore0110@gmail.com>
2023-02-11 17:17:26 +0530

    configure LSP

---
+++ lua/thesolo/lsp/init.lua
@@ -0,0 +1,8 @@
+local status_ok, _ = pcall(require, "lspconfig")
+if not status_ok then
+  return
+end
+
+require "thesolo.lsp.mason"
+require("thesolo.lsp.handlers").setup()
+require "thesolo.lsp.null-ls"
--- lua/thesolo/plugins.lua
+++ lua/thesolo/plugins.lua
@@ -58,8 +58,14 @@ return packer.startup(function(use)
 	use { "hrsh7th/cmp-nvim-lua"}
 
     -- Snippets
-    use { "L3MON4D3/LuaSnip", commit = "8f8d493e7836f2697df878ef9c128337cbf2bb84" } --snippet engine
-    use { "rafamadriz/friendly-snippets", commit = "2be79d8a9b03d4175ba6b3d14b082680de1b31b1" } -- a bunch of snippets to use
+    use { "L3MON4D3/LuaSnip"} --snippet engine
+    use { "rafamadriz/friendly-snippets"} -- a bunch of snippets to use
+
+    -- LSP
+    use "neovim/nvim-lspconfig" -- enable LSP
+    use "williamboman/mason.nvim" -- simple to use language server installer
+    use "williamboman/mason-lspconfig.nvim" -- simple to use language server installer
+    use 'jose-elias-alvarez/null-ls.nvim' -- LSP diagnostics and code actions
 
     -- Automatically set up your configuration after cloning packer.nvim
     -- Put this at the end after all plugins
