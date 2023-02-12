7d9b613135dd85f564d9a1bddbe126a5e1e89679
Gambhir <grathore0110@gmail.com>
2023-02-12 20:42:41 +0530

    configure nvim-tree plugini and keymap

--- lua/thesolo/plugins.lua
+++ lua/thesolo/plugins.lua
@@ -89,6 +89,10 @@ return packer.startup(function(use)
     --Git
     use "lewis6991/gitsigns.nvim"
 
+    --nvim-tree
+    use { "kyazdani42/nvim-tree.lua"}
+    use { "kyazdani42/nvim-web-devicons"}
+
     -- Automatically set up your configuration after cloning packer.nvim
     -- Put this at the end after all plugins
     if PACKER_BOOTSTRAP then
