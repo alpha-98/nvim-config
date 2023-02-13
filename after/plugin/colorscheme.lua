eb1e38cff6dd28642d7d645fcaa57fb69de2fb26
Gambhir <grathore0110@gmail.com>
2023-02-14 01:32:55 +0530

    disable transparent for sometime need to be fixed

--- after/plugin/colorscheme.lua
+++ after/plugin/colorscheme.lua
@@ -5,7 +5,7 @@ if not status_ok then
 	return
 end
 
-vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
-vim.api.nvim_set_hl(0, "NormalSB", { bg = "none" })
+-- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+-- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+-- vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
+-- vim.api.nvim_set_hl(0, "NormalSB", { bg = "none" })
